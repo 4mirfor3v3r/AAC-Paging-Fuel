c5502ae98039fb9ac14216cd7877913295bbd4ba
ahmad_amirudin <4mirfor3v3r@gmail.com>
2020-07-06 09:58:20 +0700

    Clean Architecture only network 🌏

--- app/src/main/java/com/acemirr/training_task_1/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.acemirr.training_task_1.utils
-
-object Constants {
-    const val TAG_DEBUG = "result_debug"
-    const val TAG_ERROR = "result_error"
-    const val EXTRA_DATA_PAGING = "datapaging"
-}
--- app/src/main/java/com/acemirr/training_task_1/utils/LoadingState.kt
+++
@@ -1,5 +0,0 @@
-package com.acemirr.training_task_1.utils
-
-enum class LoadingState {
-    DONE, LOADING
-}
